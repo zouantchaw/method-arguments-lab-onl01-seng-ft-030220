0900f77279976963605dd06dc7e44c3710f0768d
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-04 18:26:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,9 @@ def introduction(name)
 end 
 
 introduction("Josh")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Dan", "Ember.js")
